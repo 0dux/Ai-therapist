57c906be6a908162a1c32e9bc699b641e3148dde
Daksh Yadav <dakshdav@gmail.com>
2025-09-21 16:14:43 +0530

    Project is setup

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <div className="w-full fixed top-0z-50 bg-background/95 backdrop-blur-md">
+        <div className="absolute inset-0 border-b border-primary/10"/>
+        <h1>Header</h1>
+    </div>
+  )
+}
+
+export default Header
