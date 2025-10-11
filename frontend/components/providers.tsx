59c13efb0e1addf285790a1c538cdadf8396561d
Daksh Yadav <dakshdav@gmail.com>
2025-10-12 00:02:53 +0530

    almost finished

--- frontend/components/providers.tsx
+++ frontend/components/providers.tsx
@@ -1,8 +1,15 @@
 import { ReactNode } from "react";
 import ThemeProvider from "./themeProvider";
-
+import { SessionProvider as NextauthSessionProvider } from "next-auth/react";
+import { SessionProvider as CustomSessionProvider } from "../lib/context/session-context";
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <NextauthSessionProvider>
+      <CustomSessionProvider>
+        <ThemeProvider>{children}</ThemeProvider>;
+      </CustomSessionProvider>
+    </NextauthSessionProvider>
+  );
 };
 
 export default Providers;
